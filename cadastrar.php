edc72e910c0bd5e3d54a295a59d3eb717273c2da
AdilaSouza <66882157+AdilaSouza@users.noreply.github.com>
2020-10-23 08:34:16 -0300

    primeiro commit

---
+++ cadastrar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Projeto de Login</title>
+    <link rel="stylesheet" type="text/css" href="CSS/estilo2.css"/>
+
+</head>
+    <body>
+       <div id="corpo-form-cad"> 
+          <h1>Cadastrar </h1>
+
+             <form method="POST">
+                     <input type="text" placeholder="Nome Completo" class="inpt" style="text-align: center">
+                     <input type="text" placeholder="Telefone" class="inpt" style="text-align: center">
+                     <input type="text" placeholder="Usuário" class="inpt" style="text-align: center">
+                     <input type="password" placeholder="Senha" class="inpt" style="text-align: center">
+                     <input type="password" placeholder="Confimar Senha" class="inpt" style="text-align: center">
+
+                     <input type="submit" value="Acessar">
+             </form> 
+       </div>
+<?php
+
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Projeto de Login</title>
+    <link rel="stylesheet" type="text/css" href="CSS/estilo2.css"/>
+
+</head>
+    <body>
+       <div id="corpo-form"> 
+          <h1>Entrar </h1>
+
+             <form method="POST" action="processa.php" >
+                     <input type="email" placeholder="Usuário" class="inpt" style="text-align: center">
+                     <input type="password" placeholder="Senha" class="inpt" style="text-align: center">
+                     <input type="submit" value="Acessar">
+                     <a href="cadastrar.php"> Ainda não é inscrito?  <strong> Cadastre-se! </strong> </a>
+             </form> 
+       </div>
+</body>
+</html>
